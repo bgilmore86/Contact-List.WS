698f762a3b32f38ab3d64bc8a36d0dd912ce8e01
Barb T <stayyounng86@gmail.com>
2024-01-12 19:13:19 -0600

    add contactlist component

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,20 @@
+export default function ContactList() {
+    return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {}
+             
+          </tbody>
+        </table>
+    ); 
+}
