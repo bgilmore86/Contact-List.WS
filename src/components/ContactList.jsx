c43b5cbba5485b845c43620c36dace710d7d7549
Barb T <stayyounng86@gmail.com>
2024-01-14 01:07:06 -0600

    update 2 fetch API sucessful ContactList

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import ContactRow from './ContactRow';
-
+import {useEffect} from 'react'
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -11,7 +11,23 @@ const dummyContacts = [
 export default function ContactList() {
 
   const [contacts, setContacts] = useState(dummyContacts);
-
+      useEffect (() => {
+        async function fetchContacts() {
+           try{
+            //fetch API to make GET request to API endpoint
+            const response = await fetch ("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+            const data = await response.json();
+            //setState function to update state of contacts
+            setContacts(data);
+             } catch (error) {
+            console.error(error);
+            alert("Error with Request"); //will log any errors in retrieving API request
+          }
+        }
+        fetchContacts ();
+      },[]
+      );
+    
     return ( 
         <table>
           <thead>
